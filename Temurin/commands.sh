378a5ad051321294545ee2a57bbddd99a340aba0
gizzi <gianluca.gizzi@cefriel.com>
2024-04-12 11:09:36 +0200

    Added project source folder and updated docker-compose.yaml, Dockerfile, and commands.sh for the Temurin example

--- Temurin/commands.sh
+++ Temurin/commands.sh
@@ -4,7 +4,7 @@
 sleep 3
 
 # Start the Java application in the background
-java -Xmx1024m -jar /home/ChimeraCamelJava-executable.jar &
+java -Xmx1024m -jar /home/minimal-chimera.jar &
 
 # Log header to CSV file
 echo "Timestamp,MemoryUsage(MB),CPUUsage" > /home/out/stats.txt
@@ -14,12 +14,12 @@ while true; do
     timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
     # Get memory usage in kilobytes
-    memory_usage_kb=$(ps -o rss -p $(pgrep -d',' -f "java -Xmx1024m -jar /home/ChimeraCamelJava-executable.jar") | tail -n 1)
+    memory_usage_kb=$(ps -o rss -p $(pgrep -d',' -f "java -Xmx1024m -jar /home/minimal-chimera.jar") | tail -n 1)
 
     # Convert kilobytes to megabytes (direct arithmetic expression)
     memory_usage_mb=$((memory_usage_kb / 1024))
 
-    cpu_usage=$(ps -o pcpu -p $(pgrep -d',' -f "java -Xmx1024m -jar /home/ChimeraCamelJava-executable.jar") | tail -n 1)
+    cpu_usage=$(ps -o pcpu -p $(pgrep -d',' -f "java -Xmx1024m -jar /home/minimal-chimera.jar") | tail -n 1)
 
     # Append stats to CSV file
     echo "$timestamp,$memory_usage_mb,$cpu_usage" >> /home/out/stats.txt
