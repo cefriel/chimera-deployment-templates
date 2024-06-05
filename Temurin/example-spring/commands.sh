9845db59188ab875a9f96edf68819c42a0369c65
gizzi <gianluca.gizzi@cefriel.com>
2024-06-05 14:46:40 +0200

    Added docker files for building and running a minima chimera app developed using spring

---
+++ Temurin/example-spring/commands.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Wait a little bit to ensure volumes are mounted correctly
+sleep 3
+
+# Start the Java application in the background
+java -Xmx1024m -jar /home/minimal-chimera-spring-0.0.2.jar &
+
+# Log header to CSV file
+echo "Timestamp,MemoryUsage(MB),CPUUsage" > /home/out/stats.txt
+
+# Log stats every second indefinitely
+while true; do
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+    # Get memory usage in kilobytes
+    memory_usage_kb=$(ps -o rss -p $(pgrep -d',' -f "java -Xmx1024m -jar /home/minimal-chimera-spring-0.0.2.jar") | tail -n 1)
+
+    # Convert kilobytes to megabytes (direct arithmetic expression)
+    memory_usage_mb=$((memory_usage_kb / 1024))
+
+    cpu_usage=$(ps -o pcpu -p $(pgrep -d',' -f "java -Xmx1024m -jar /home/minimal-chimera-spring-0.0.2.jar") | tail -n 1)
+
+    # Append stats to CSV file
+    echo "$timestamp,$memory_usage_mb,$cpu_usage" >> /home/out/stats.txt
+
+    sleep 5
+done
